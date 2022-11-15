81c513a36b7cb6e5d23a3096f38e996e15ad43d7
GigihDC <112244673+GigihDC@users.noreply.github.com>
2022-11-15 22:45:06 +0700

    Add files via upload

---
+++ minggu_10_moviedb/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,19 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Mobile\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "E:\\file mobile programming\\minggu_10_moviedb" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Mobile\\flutter"
+  "PROJECT_DIR=E:\\file mobile programming\\minggu_10_moviedb"
+  "FLUTTER_ROOT=C:\\Mobile\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=E:\\file mobile programming\\minggu_10_moviedb\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=E:\\file mobile programming\\minggu_10_moviedb"
+  "FLUTTER_TARGET=E:\\file mobile programming\\minggu_10_moviedb\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "EXTRA_FRONT_END_OPTIONS=--no-sound-null-safety"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=E:\\file mobile programming\\minggu_10_moviedb\\.dart_tool\\package_config.json"
+)
